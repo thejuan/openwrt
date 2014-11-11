022dee3d521f7db4d511618e0692f84406228e83
adam <adam@chambills.com>
2014-11-11 14:29:57 +1100

    Adding pip support

--- packages/python-ipset/src/setup.py
+++ packages/python-ipset/src/setup.py
@@ -5,17 +5,26 @@
 from distutils.core import setup, Extension
 
 # make pyflakes happy
-__pkgname__ = None
+__pkgname__ = "ipset"
 __version__ = None
 
-# build/install python-iptables
+# build/install 
 setup(
     name=__pkgname__,
     version=__version__,
-    description="Python bindings for ipset",
+    description="Python bindings for adding/removing to an ipset",
     py_modules = ['ipset'],
     ext_modules=[Extension("_ipset",
                            ["ipset.c", "ipset.i"])],
-    
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache License',
+        'Operating System :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     license="Apache License, Version 2.0",
 )
